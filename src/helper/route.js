2c684580c4fbfaf696eb76e2d2310373a9aa68dd
samaritan89 <sunluyong@gmail.com>
2017-09-02 19:33:07 +0800

    feat: file list

--- src/helper/route.js
+++ src/helper/route.js
@@ -1,7 +1,14 @@
 const fs = require('fs');
+const path = require('path');
+const Handlebars = require('handlebars');
 const promisify = require('util').promisify;
 const stat = promisify(fs.stat);
 const readdir = promisify(fs.readdir);
+const config = require('../config/defaultConfig');
+
+const tplPath = path.join(__dirname, '../template/dir.tpl');
+const source = fs.readFileSync(tplPath);
+const template = Handlebars.compile(source.toString());
 
 module.exports = async function (req, res, filePath) {
   try {
@@ -11,14 +18,21 @@ module.exports = async function (req, res, filePath) {
       res.setHeader('Content-Type', 'text/plain');
       fs.createReadStream(filePath).pipe(res);
     } else if (stats.isDirectory()) {
-      const files = readdir(filePath);
+      const files = await readdir(filePath);
       res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/plain');
-      res.end(files.join(','));
+      res.setHeader('Content-Type', 'text/html');
+      const dir = path.relative(config.root, filePath);
+      const data = {
+        title: path.basename(filePath),
+        dir: dir ? `/${dir}` : '',
+        files
+      };
+      res.end(template(data));
     }
   } catch (ex) {
+    console.error(ex);
     res.statusCode = 404;
     res.setHeader('Content-Type', 'text/plain');
-    res.end(`${filePath} is not a directory or file`);
+    res.end(`${filePath} is not a directory or file\n ${ex.toString()}`);
   }
 }
---
+++ src/template/dir.tpl
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{title}}</title>
+    <style>
+        body {
+            margin: 30px;
+        }
+
+        a {
+            display: block;
+            font-size: 30px;
+        }
+    </style>
+</head>
+<body>
+{{#each files}}
+    <a href="{{../dir}}/{{this}}">{{this}}</a>
+{{/each}}
+</body>
+</html>
