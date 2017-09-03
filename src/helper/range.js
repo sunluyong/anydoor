14158a200188fde3bd2ddfdbcf74b2c6e022b322
samaritan89 <sunluyong@gmail.com>
2017-09-03 15:11:30 +0800

    feat: range

---
+++ src/helper/range.js
@@ -0,0 +1,23 @@
+module.exports = (totalSize, req, res) => {
+  const range = req.headers['range'];
+  if (!range) {
+    return {code: 200};
+  }
+
+  const sizes = range.match(/bytes=(\d*)-(\d*)/);
+  const end = sizes[2] || totalSize - 1;
+  const start = sizes[1] || totalSize - end;
+
+  if (start > end || start < 0 || end > totalSize) {
+    return {code: 200};
+  }
+
+  res.setHeader('Accept-Ranges', 'bytes');
+  res.setHeader('Content-Range', `bytes ${start}-${end}/${totalSize}`);
+  res.setHeader('Content-Length', end - start);
+  return {
+    code: 206,
+    start: parseInt(start),
+    end: parseInt(end)
+  };
+};
--- src/helper/route.js
+++ src/helper/route.js
@@ -7,6 +7,7 @@ const readdir = promisify(fs.readdir);
 const config = require('../config/defaultConfig');
 const mime = require('./mime');
 const compress = require('./compress');
+const range = require('./range');
 
 const tplPath = path.join(__dirname, '../template/dir.tpl');
 const source = fs.readFileSync(tplPath);
@@ -17,9 +18,16 @@ module.exports = async function (req, res, filePath) {
     const stats = await stat(filePath);
     if (stats.isFile()) {
       const contentType = mime(filePath);
-      res.statusCode = 200;
       res.setHeader('Content-Type', contentType);
-      let rs = fs.createReadStream(filePath);
+      let rs;
+      const {code, start, end} = range(stats.size, req, res);
+      if (code === 200) {
+        res.statusCode = 200;
+        rs = fs.createReadStream(filePath);
+      } else {
+        res.statusCode = 206;
+        rs = fs.createReadStream(filePath, {start, end});
+      }
       if (filePath.match(config.compress)) {
         rs = compress(rs, req, res);
       }
