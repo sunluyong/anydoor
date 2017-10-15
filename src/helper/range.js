0d210059979c10c80fd5736223b4bc4905c756e1
Samaritain <sunluyong@gmail.com>
2017-10-15 05:28:21 -0500

    Update range.js

--- src/helper/range.js
+++ src/helper/range.js
@@ -5,8 +5,8 @@ module.exports = (totalSize, req, res) => {
   }
 
   const sizes = range.match(/bytes=(\d*)-(\d*)/);
-  const end = sizes[2] || totalSize - 1;
-  const start = sizes[1] || totalSize - end;
+  const end = sizes[2]? parseInt(sizes[2]) : totalSize - 1;
+  const start = sizes[1]? parseInt(sizes[1]) : totalSize - end;
 
   if (start > end || start < 0 || end > totalSize) {
     return {code: 200};
@@ -17,7 +17,7 @@ module.exports = (totalSize, req, res) => {
   res.setHeader('Content-Length', end - start);
   return {
     code: 206,
-    start: parseInt(start),
-    end: parseInt(end)
+    start: start,
+    end: end
   };
 };
